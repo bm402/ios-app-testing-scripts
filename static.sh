129240b5fff485c02b7c6b0ceeaf1643304341a5
bncrypted <bncrypted@gmail.com>
2020-08-20 15:07:13 +0100

    class dumps

--- static.sh
+++ static.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 # Functions
-function printToFileWithHeading {
+function printTitle {
+    echo >> "$outputfile"
     echo "[*] $1" >> "$outputfile"
     echo >> "$outputfile"
-    echo "$2" >> "$outputfile"
-    echo >> "$outputfile"
+    echo
+    echo "[*] $1"
+    echo
 }
 
-function printToStdoutWithHeading {
+function printContentWithHeading {
+    echo "$1:" >> "$outputfile"
+    echo "$2" >> "$outputfile"
+    echo >> "$outputfile"
     echo "$1:"
     echo "$2"
     echo
 }
 
-function printWithHeadings {
-    printToFileWithHeading "$1" "$2"
-    printToStdoutWithHeading "$1" "$2"
+function printContent {
+    echo "$1" >> "$outputfile"
+    echo >> "$outputfile"
+    echo "$1"
+    echo
 }
 
 function plistXmlToJson {
@@ -36,7 +43,6 @@ fi
 
 # Navigate to directory
 curdir=`pwd`
-echo "[*] Finding app on the filesystem"
 cd "$HOME/Documents/ios-apps/$1/AppFiles"
 
 # Create output file
@@ -45,19 +51,45 @@ outputfile="$curdir/static-analysis-$1-$timestamp.txt"
 touch "$outputfile"
 
 # Find Info.plist, convert to JSON and print
-echo "[*] Getting Info.plist"
-echo
+printTitle "Info.plist"
 plistjson=
 plistXmlToJson "Info.plist"
-printWithHeadings "Info.plist" "$plistjson"
+printContent "$plistjson"
+infoplistjson="$plistjson"
 
 # Find plist files, convert to JSON and print
-echo "[*] Finding other .plist files"
+printTitle "Other .plist files"
 plists=`find . -name "*.plist" | grep -v -E ".storyboardc|/Frameworks/|./Info.plist"`
-echo
 for plist in $plists; do
     plistjson=
     plistXmlToJson "$plist"
     plistname=`echo "$plist" | cut -c 3-`
-    printWithHeadings "$plistname" "$plistjson"
+    printContentWithHeading "$plistname" "$plistjson"
 done
+
+# Class dumps
+words=(Debug Test Dummy Old Legacy Secret Key Encrypt Encode Decrypt Decode)
+
+printTitle "Interesting objc classes"
+dumpfileobjc="class-dump-objc-$1-$timestamp.txt"
+dsdump --objc --verbose=0 --arch arm64 --defined "$HOME/Documents/ios-apps/$1/AppFiles/$1" > "$dumpfileobjc"
+dsdump --objc --verbose=5 --arch arm64 --defined "$HOME/Documents/ios-apps/$1/AppFiles/$1" > "$curdir/$dumpfileobjc"
+for word in "${words[@]}"; do
+    classes=`grep -iF "$word" "$dumpfileobjc"`
+    if [ ! -z "$classes" ]; then
+        printContentWithHeading "$word" "$classes"
+    fi
+done
+rm "$dumpfileobjc"
+
+printTitle "Interesting swift classes"
+dumpfileswift="class-dump-swift-$1-$timestamp.txt"
+dsdump --swift --verbose=0 --arch arm64 --defined "$HOME/Documents/ios-apps/$1/AppFiles/$1" > "$dumpfileswift"
+dsdump --swift --verbose=5 --arch arm64 --defined "$HOME/Documents/ios-apps/$1/AppFiles/$1" > "$curdir/$dumpfileswift"
+for word in "${words[@]}"; do
+    classes=`grep -iF "$word" "$dumpfileswift"`
+    if [ ! -z "$classes" ]; then
+        printContentWithHeading "$word" "$classes"
+    fi
+done
+rm "$dumpfileswift"
